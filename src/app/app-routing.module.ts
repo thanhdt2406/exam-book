4b03b508c9a9d9bedb9e589177327f96f25e3fb4
thanhdt246 <dothanh24062000@gmail.com>
2021-01-07 16:11:57 +0700

    submit exam

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ListBookComponent} from "./book/list-book/list-book.component";
+import {CreatBookComponent} from "./book/creat-book/creat-book.component";
+import {EditBookComponent} from "./book/edit-book/edit-book.component";
+import {DetailBookComponent} from "./book/detail-book/detail-book.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'books',
+    component: ListBookComponent,
+  },
+  {
+    path: 'books/create',
+    component: CreatBookComponent
+  },
+  {
+    path: 'books/edit/:id',
+    component: EditBookComponent
+  },
+  {
+    path: 'books/:id',
+    component: DetailBookComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListBookComponent } from './book/list-book/list-book.component';
+import { EditBookComponent } from './book/edit-book/edit-book.component';
+import { CreatBookComponent } from './book/creat-book/creat-book.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import { DetailBookComponent } from './book/detail-book/detail-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListBookComponent,
+    EditBookComponent,
+    CreatBookComponent,
+    DetailBookComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
